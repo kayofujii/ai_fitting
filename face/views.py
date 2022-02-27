6261f5c8e3b7a7f104f8a149e373a020e8c47e47
kayofujii <kayosamu1025@gmail.com>
2022-02-27 13:45:33 +0900

    回転情報があれば、写真を回転するようにした

--- face/views.py
+++ face/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 import cv2
 import numpy as np
+import pyheif
 from azure.cognitiveservices.vision.face import FaceClient
 from django.conf import settings
 from django.contrib import messages
@@ -46,10 +47,10 @@ def upload_image(request):
         if form.is_valid():
             user_im = request.FILES.get("user_im")
             product_im = request.FILES.get("product_im")
-            hash_now_date, output = recognize_face_with_api(
-                user_im, product_im)
-
-            if not output:
+            try:
+                hash_now_date, output = recognize_face_with_api(
+                    user_im, product_im)
+            except:
                 messages.error(request, '画像の作成に失敗しました。')
                 return redirect('index')
 
@@ -61,10 +62,79 @@ def upload_image(request):
             return redirect('index')
 
 
-def get_tmp_image_path(dir, hash_now_date):
+def get_tmp_image_path(dir, hash_now_date, im=None):
+    extention = '.png'
+    if im:
+        extention = os.path.splitext(im.name)[1]
     os.makedirs(str(settings.BASE_DIR) +
                 f"/media/tmp/{dir}{hash_now_date}", exist_ok=True)
-    return f"/media/tmp/{dir}{hash_now_date}/%s%s" % (hash_now_date, ".png")
+    return f"/media/tmp/{dir}{hash_now_date}/%s%s" % (hash_now_date, extention)
+
+
+def convert_heif_to_png(image_path):
+    new_name = image_path.replace('HEIC', 'png')
+    print('media/' + image_path)
+    heif_file = pyheif.read('media/' + image_path)
+    print(heif_file)
+    data = Image.frombytes(
+        heif_file.mode,
+        heif_file.size,
+        heif_file.data,
+        "raw",
+        heif_file.mode,
+        heif_file.stride,
+    )
+    data.save('media/' + new_name, "PNG")
+    return new_name
+
+
+def rotateImage(img, orientation):
+    """
+    画像ファイルをOrientationの値に応じて回転させる
+    """
+    # orientationの値に応じて画像を回転させる
+    if orientation == 1:
+        pass
+    elif orientation == 2:
+        # 左右反転
+        img_rotate = img.transpose(Image.FLIP_LEFT_RIGHT)
+    elif orientation == 3:
+        # 180度回転
+        img_rotate = img.transpose(Image.ROTATE_180)
+    elif orientation == 4:
+        # 上下反転
+        img_rotate = img.transpose(Image.FLIP_TOP_BOTTOM)
+    elif orientation == 5:
+        # 左右反転して90度回転
+        img_rotate = img.transpose(
+            Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_90)
+    elif orientation == 6:
+        # 270度回転
+        img_rotate = img.transpose(Image.ROTATE_270)
+    elif orientation == 7:
+        # 左右反転して270度回転
+        img_rotate = img.transpose(
+            Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_270)
+    elif orientation == 8:
+        # 90度回転
+        img_rotate = img.transpose(Image.ROTATE_90)
+    else:
+        pass
+
+    return img_rotate
+
+
+def save_image_match_exif(img, filename):
+    # exif情報取得
+    exifinfo = img._getexif()
+    # exif情報からOrientationの取得
+    orientation = exifinfo.get(0x112, 1)
+    # 画像を回転
+    img_rotate = rotateImage(img, orientation)
+    # 回転した画像を保存（元の画像に上書き）
+    img_rotate.save('media/' + filename)
+    img = Image.open('media/' + filename)
+    return img
 
 
 def recognize_face_with_api(user_im, product_im):
@@ -74,11 +144,17 @@ def recognize_face_with_api(user_im, product_im):
     now_date = str(datetime.now())
     hash_now_date = hashlib.sha1((now_date).encode("utf-8")).hexdigest()
 
-    u_path = str(settings.BASE_DIR) + get_tmp_image_path('user', hash_now_date)
+    u_path = str(settings.BASE_DIR) + \
+        get_tmp_image_path('user', hash_now_date, user_im)
     fs = FileSystemStorage()
     filename = fs.save(u_path, user_im)
 
+    # if os.path.splitext(filename)[1] == '.HEIC':
+    #     filename = convert_heif_to_png(filename)
     img = Image.open('media/' + filename)
+    if img._getexif():
+        img = save_image_match_exif(img, filename)
+
     u_stream = open(
         'media/' + filename, "rb")
 
@@ -109,12 +185,17 @@ def recognize_face_with_api(user_im, product_im):
         get_tmp_image_path('crop', hash_now_date)
     im_rgba_crop.save(crop_path)
 
-    p_path = str(settings.BASE_DIR) + get_tmp_image_path('pro', hash_now_date)
+    p_path = str(settings.BASE_DIR) + \
+        get_tmp_image_path('pro', hash_now_date, product_im)
     ps = FileSystemStorage()
     pfilename = ps.save(p_path, product_im)
 
     # 商品画像の顔を識別
     pro_img = Image.open('media/' + pfilename)
+
+    if pro_img._getexif():
+        pro_img = save_image_match_exif(pro_img, filename)
+
     p_stream = open('media/' + pfilename, "rb")
 
     p_detected_faces = face_client.face.detect_with_stream(
