5c816caee29490f7bdc66763e7e99ef1a447f12d
kayofujii <kayosamu1025@gmail.com>
2022-03-12 18:58:20 +0900

    edit signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from .forms import SignUpForm
+
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    form_class = SignUpForm
+    success_url = reverse_lazy('user_info')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
